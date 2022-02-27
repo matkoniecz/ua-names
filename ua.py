5e2a6ad191b9a7ee0ade7d420513e098bfb9c40e
Mateusz Konieczny <matkoniecz@gmail.com>
2022-02-27 19:39:28 +0100

    better data handling and reporting

--- ua.py
+++ ua.py
@@ -3,10 +3,13 @@ import csv
 
 import osm_bot_abstraction_layer.osm_bot_abstraction_layer as osm_bot_abstraction_layer
 
-def main():
-    for file in ['szpitale.csv', 'pks.csv', 'krew.csv', 'community_centre.csv', 'miasta.csv']: #['miasta_unified.csv']:
+def main(make_edits):
+    report_of_problems = ""
+    for file in ['krew.csv', 'szpitale.csv', 'pks.csv', 'community_centre.csv', 'miasta.csv']:
+        report_of_problems += "\n------\n" + file + "\n"
         show_overpass_query(file)
-        changeset = build_changeset(False, "dodawanie nazw ukraińskich i rosyjskich (edycje osób które zgodziły się na dystrybucje ich pracy ale edytowały listę nazw a nie bezpośrednio w edytorze OSM)", "https://forum.openstreetmap.org/viewtopic.php?id=75019", "https://wiki.openstreetmap.org/wiki/Mechanical_Edits/Mateusz_Konieczny_-_bot_account/name_translation_proxy_ukraine_refugee_crisis")
+        if make_edits:
+            changeset = build_changeset(False, "dodawanie nazw ukraińskich i rosyjskich (edycje osób które zgodziły się na dystrybucje ich pracy ale edytowały listę nazw a nie bezpośrednio w edytorze OSM)", "https://forum.openstreetmap.org/viewtopic.php?id=75019", "https://wiki.openstreetmap.org/wiki/Mechanical_Edits/Mateusz_Konieczny_-_bot_account/name_translation_proxy_ukraine_refugee_crisis")
         with open(file, newline='') as csvfile:
             print("aaa")
             spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
@@ -26,37 +29,68 @@ def main():
                     if matcher.get("name:uk") != None:
                         name_uk = row[matcher.get("name:uk")]
                     element = osm_bot_abstraction_layer.get_data(osm_element_id, osm_element_type)
+                    if element == None:
+                        error = "NO ELEMENT AT ALL ( " + link + " )!\n"
+                        report_of_problems += error
+                        print(error)
+                        skip_edit = True
+                        continue # no need to check anything else
+                    if "tag" not in element:
+                        error = "NO TAGS AT ALL ( " + link + " )!\n"
+                        report_of_problems += error
+                        print(error)
+                        skip_edit = True
+                        continue # no need to check anything else
                     tags = element["tag"]
+
+                    skip_edit = False
+                    link = "https://www.openstreetmap.org/" + osm_element_type + "/" + osm_element_id
                     if "name" not in tags:
-                        print("NAME MISSING!")
-                        print("https://www.openstreetmap.org/" + osm_element_type + "/" + osm_element_id)
-                        continue
-                    if name != tags["name"]:
-                        print("https://www.openstreetmap.org/" + osm_element_type + "/" + osm_element_id)
-                        print("NAME MISMATCH!")
-                        print("Translated:")
-                        print(name)
-                        print()
-                        print("in OSM:")
-                        print(tags["name"])
-                        print()
-                        print()
-                        print()
-                        print()
-                        continue
+                        error = "NAME MISSING ( " + link + " )!\n"
+                        report_of_problems += error
+                        print(error)
+                        skip_edit = True
+                    elif name != tags["name"]:
+                        expanded_name = tags["name"]
+                        if "RCKiK" in tags["name"]:
+                            expanded_name = expanded_name.replace("RCKiK", "Regionalne Centrum Krwiodawstwa i Krwiolecznictwa")
+                            expanded_name = expanded_name.replace("TO", "Oddział Terenowy")
+                            expanded_name = expanded_name.replace("OT", "Oddział Terenowy")
+                        if "WCKiK" in tags["name"]:
+                            expanded_name = expanded_name.replace("WCKiK", "Wojskowe Centrum Krwiodawstwa i Krwiolecznictwa")
+
+                        if name != expanded_name:
+                            error = "NAME MISMATCH ( " + link + " )!\n"
+                            error += "Being translated:\n" + name + "\n"
+                            error += "In OSM:\n" + tags["name"] + "\n"
+                            error += "expanded_name:\n" + expanded_name + "\n\n"
+
+                            report_of_problems += error
+                            print(error)
+                            skip_edit = True
                     if name_uk != None:
-                        if "name:uk" in tags and name_uk != tags["name:uk"]:
-                            print("NAME MISMATCH! name:uk")
-                            print(name_uk, "vs", tags["name:uk"])
-                            continue
+                        if "name:uk" in tags and name_uk.strip() != tags["name:uk"].strip():
+                            error = "NAME MISMATCH ( " + link + " )!\n"
+                            error += "Provided:\n" + name_uk.strip() + "\n"
+                            error += "In OSM:\n" + tags["name:uk"].strip() + "\n\n"
+
+                            report_of_problems += error
+                            print(error)
+                            skip_edit = True
                     if name_ru != None:
-                        if "name:ru" in tags and name_ru != tags["name:ru"]:
-                            print("NAME MISMATCH! name:ru")
-                            print(name_ru, "vs", tags["name:ru"])
-                            continue
+                        if "name:ru" in tags and name_ru.strip() != tags["name:ru"].strip():
+                            error = "NAME MISMATCH ( " + link + " )!\n"
+                            error += "Provided:\n" + name_ru.strip() + "\n"
+                            error += "In OSM:\n" + tags["name:ru"].strip() + "\n\n"
 
+                            report_of_problems += error
+                            print(error)
+                            skip_edit = True
+
+                    if make_edits == False or skip_edit:
+                        continue
+    
                     useful_edit = False
-
                     # may be not needed anymore?
                     if "name:ru" in tags and element["tag"]["name:ru"].strip() == "":
                         useful_edit = True
@@ -68,12 +102,12 @@ def main():
                         print("cleaning empty name! Sorry!")
 
                     if name_ru != None and name_ru.strip() != "":
-                        if ("name:ru" not in tags or element["tag"]["name:ru"] != name_ru):
-                            element["tag"]["name:ru"] = name_ru
+                        if ("name:ru" not in tags or element["tag"]["name:ru"] != name_ru.strip()):
+                            element["tag"]["name:ru"] = name_ru.strip()
                             useful_edit = True
                     if name_uk != None and name_uk.strip() != "":
-                        if ("name:uk" not in tags or element["tag"]["name:uk"] != name_uk):
-                            element["tag"]["name:uk"] = name_uk
+                        if ("name:uk" not in tags or element["tag"]["name:uk"] != name_uk.strip()):
+                            element["tag"]["name:uk"] = name_uk.strip()
                             useful_edit = True
 
                     if useful_edit == False:
@@ -83,12 +117,14 @@ def main():
                     continue
                 row_index += 1
         try:
-            changeset.ChangesetClose()
+            if make_edits:
+                changeset.ChangesetClose()
         except osmapi.ApiError as e:
             if is_exception_about_already_closed_changeset(e):
                 pass
             else:
                 raise e
+    print(report_of_problems)
 
 def build_id_to_index_number(header_row_array):
     matcher = {}
@@ -129,7 +165,7 @@ def show_overpass_query(file):
             row_index += 1
         print(overpass_query_builder(query_data_rows))
 
-def overpass_query(query_data_rows):
+def overpass_query_builder(query_data_rows):
     overpass_query = """
             [out:xml][timeout:2500];
     (
@@ -160,4 +196,4 @@ def build_changeset(is_in_manual_mode, changeset_comment, discussion_url, osm_wi
     builder.create_changeset(api)
     return api
 
-main()
+main(make_edits=True)
